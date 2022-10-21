4441d34451f60e6c742a42f3f3f8a481bda6f991
artiombic <chrisandartiom@gmail.com>
2022-10-21 18:10:59 -0400

    wip

---
+++ src/project/artiom.py
@@ -0,0 +1,10 @@
+class Artiom:
+    def __init__(self,a):
+        print("Peepee")
+        self.a = a
+    @staticmethod
+    def sum(a, b):
+        return a + b
+
+    def sum2(self,b):
+        return b + self.a
